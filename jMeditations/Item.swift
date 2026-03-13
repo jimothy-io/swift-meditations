b138614ca8d90a9ab3e14aa8b7dce9c343b6ec0f
Jimothy <iojimothy@gmail.com>
2026-03-13 23:29:00 +0000

    Initial commit

---
+++ jMeditations/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  jMeditations
+//
+//  Created by j on 13/03/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ jMeditations/jMeditationsApp.swift
@@ -0,0 +1,32 @@
+//
+//  jMeditationsApp.swift
+//  jMeditations
+//
+//  Created by j on 13/03/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct jMeditationsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
